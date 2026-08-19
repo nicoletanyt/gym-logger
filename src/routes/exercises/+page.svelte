d41d11b8e70ea10f50113217682b87bba75fe6a8
nicole <ntytomg@gmail.com>
2026-08-19 22:23:14 +0800

    added import feature

--- src/routes/exercises/+page.svelte
+++ src/routes/exercises/+page.svelte
@@ -70,6 +70,9 @@
             <Field label="Image Link">
                 <Input bind:value={newExercise.imageLink} />
             </Field>
+            <Field label="Note">
+                <Input bind:value={newExercise.note} />
+            </Field>
         </section>
 
         <ActionButton
